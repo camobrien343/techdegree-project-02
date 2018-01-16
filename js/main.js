04254e688d0e4d34a4b7d4f618e2ed9525ae657c
Cam O'Brien <camobrien343@gmail.com>
2018-01-15 19:46:40 -0600

    Added JQuery and created variable to store all students in list

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const allStudents = document.querySelector('.student-list').getElementsByTagName('li');
+const studentsPerPage = 10;
+
+
+
+for ( let i=0; i < allStudents.length; i++ ) {
+	console.log(allStudents[i])
+};
+
+
+
+	
